81437c677da43561221007348f1fdb2d4d441a27
Moosa Hassan <moosa7021@gmail.com>
2020-08-05 21:26:16 +1000

    basics

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SENG1120_Assignment2__C_Sharp_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
